9fc143deb867694d0eef13a174af595ffa84f528
John <claytonsetterlund@hotmail.com>
2017-05-12 20:32:21 -0400

    added asscociate controller

--- StagingManagementSystem/src/com/revature/controllers/AddAssociateContoller.java
+++ StagingManagementSystem/src/com/revature/controllers/AddAssociateContoller.java
@@ -1,7 +1,11 @@
 package com.revature.controllers;
 
+import com.revature.classes.*;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,8 +13,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
+
 @Controller
 public class AddAssociateContoller {
+	
+	//ApplicationContext apc = new FileSystemXmlApplicationContext("appContext.xml");
+	//DAOImpl dao = (DAOImpl) apc.getBean("DAOImpl");
 	
 	@RequestMapping(value = "/AddAssociateForm", method = RequestMethod.POST)
 	public String PostService(HttpServletRequest request) {
@@ -20,6 +29,10 @@ public class AddAssociateContoller {
 		System.out.println(param2);
 		String param3 = request.getParameter("phone");
 		System.out.println(param3);
+		
+		
+
+		//dao.methodname(paramname, paramname2);
 		
 		return "This works";
 	}
