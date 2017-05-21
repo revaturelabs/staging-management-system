7717ecd2e77550288e1e7e0a0fc8c4b55718aba5
Tim Stevens <stevenstr@comcast.net>
2017-05-21 09:28:16 -0400

    commit number 420691337

--- src/com/revature/classes/Driver.java
+++ src/com/revature/classes/Driver.java
@@ -17,7 +17,8 @@ public class Driver {
 	
 
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 				
 		SessionFactory sf = new Configuration().configure().buildSessionFactory();
 		
