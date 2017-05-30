b69b7064def343d82d44b19713c830a5eb8f8da9
Richard <richard.orr@revature.com>
2017-05-30 16:29:03 -0400

    Template project

--- src/com/revature/security/SpringSecurityInitializer.java
+++
@@ -1,8 +0,0 @@
-package com.revature.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer 
-{
-   //do nothing
-}
