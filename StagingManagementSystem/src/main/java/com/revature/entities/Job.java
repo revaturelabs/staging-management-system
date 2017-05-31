b440c85128ad2ac9a22275bd7686e930b7db2c01
Kota Delos Reyes <kdelosre>
2017-05-31 11:15:56 -0700

    job entity

--- StagingManagementSystem/src/main/java/com/revature/entities/Job.java
+++ StagingManagementSystem/src/main/java/com/revature/entities/Job.java
@@ -4,22 +4,36 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name="JOBS")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Job {
 
 	@Id
 	@Column(name="JOB_ID")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "JOB_ID_SEQ")
+	@SequenceGenerator(name = "JOB_ID_SEQ", sequenceName = "JOB_ID_SEQ")
 
 	private long id;
 	
-	@Column(name="ASSOCIATE_ID")
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="ASSOCIATE_ID")
 	private long associateId;
-	
+
 	@Column(name="CLIENT_ID")
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="CLIENT_ID")
 	private long clientId;
 	
 	@Column(name="START_DATE")
@@ -106,57 +120,6 @@ public class Job {
 		this.buyoutDate = buyoutDate;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (int) (associateId ^ (associateId >>> 32));
-		result = prime * result + ((buyoutDate == null) ? 0 : buyoutDate.hashCode());
-		result = prime * result + (int) (clientId ^ (clientId >>> 32));
-		result = prime * result + ((endDate == null) ? 0 : endDate.hashCode());
-		result = prime * result + (int) (id ^ (id >>> 32));
-		result = prime * result + ((projectedEndDate == null) ? 0 : projectedEndDate.hashCode());
-		result = prime * result + ((startDate == null) ? 0 : startDate.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Job other = (Job) obj;
-		if (associateId != other.associateId)
-			return false;
-		if (buyoutDate == null) {
-			if (other.buyoutDate != null)
-				return false;
-		} else if (!buyoutDate.equals(other.buyoutDate))
-			return false;
-		if (clientId != other.clientId)
-			return false;
-		if (endDate == null) {
-			if (other.endDate != null)
-				return false;
-		} else if (!endDate.equals(other.endDate))
-			return false;
-		if (id != other.id)
-			return false;
-		if (projectedEndDate == null) {
-			if (other.projectedEndDate != null)
-				return false;
-		} else if (!projectedEndDate.equals(other.projectedEndDate))
-			return false;
-		if (startDate == null) {
-			if (other.startDate != null)
-				return false;
-		} else if (!startDate.equals(other.startDate))
-			return false;
-		return true;
-	}
 
 	@Override
 	public String toString() {
@@ -164,6 +127,4 @@ public class Job {
 				+ ", projectedEndDate=" + projectedEndDate + ", endDate=" + endDate + ", buyoutDate=" + buyoutDate
 				+ "]";
 	}
-	
-	
 }
