90db3fc406eee69edc3b484d27003ca2d63cb843
Blake <btkruppa513@gmail.com>
2017-05-31 13:25:09 -0400

    Added associate and credential entities

--- StagingManagementSystem/src/main/java/com/revature/services/AssociateService.java
+++ StagingManagementSystem/src/main/java/com/revature/services/AssociateService.java
@@ -1,5 +1,11 @@
 package com.revature.services;
 
+import com.revature.entities.Associate;
+
 public interface AssociateService {
+
+	Associate getById(long id);
+
+	void add(Associate associate);
 
 }
