c9bf1d221fee9a2ad9e3239e1cb7210dbc628ae1
Jozsef Morrissey <jtmorri2@illinois.edu>
2017-05-31 16:27:05 -0400

    merging

---
+++ StagingManagementSystem/src/main/java/com/revature/entities/Location.java
@@ -0,0 +1,99 @@
+package com.revature.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name="LOCATIONS")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Location 
+{
+
+	@Id
+	@Column(name="LOCATION_ID")
+	long id;
+	
+	@Column
+	String country;
+	@Column
+	String state;
+	@Column
+	String city;
+	@Column
+	String name;
+	
+	public Location(){}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	@Override
+	public String toString() {
+		return "Location [id=" + id + ", country=" + country + ", state=" + state + ", city=" + city + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((country == null) ? 0 : country.hashCode());
+		result = (int) (prime * result + ((id == 0) ? 0 : id));
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Location other = (Location) obj;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (country == null) {
+			if (other.country != null)
+				return false;
+		} else if (!country.equals(other.country))
+			return false;
+		if (id != other.id) {
+				return false;
+		} 
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
+			return false;
+		return true;
+	}
+		
+}
