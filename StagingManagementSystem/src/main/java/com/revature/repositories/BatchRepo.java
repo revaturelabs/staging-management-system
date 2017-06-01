b1a0343f243f3c1fc4ae0364f4531e5b1c5c5b17
Mykola Nikitin <mykola.nikitin.1995@gmail.com>
2017-06-01 11:54:00 -0400

    Added checkin service

--- StagingManagementSystem/src/main/java/com/revature/repositories/BatchRepo.java
+++ StagingManagementSystem/src/main/java/com/revature/repositories/BatchRepo.java
@@ -1,9 +1,14 @@
 package com.revature.repositories;
 
+import com.revature.entities.BatchType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.revature.entities.Batch;
 
-public interface BatchRepo extends JpaRepository<Batch, Long> {
+import java.time.LocalDate;
+import java.util.Set;
 
+public interface BatchRepo extends JpaRepository<Batch, Long> {
+    Batch findByStartDate(LocalDate startDate);
+    Set<Batch> findAllByBatchType(BatchType batchType);
 }
---
+++ StagingManagementSystem/src/main/java/com/revature/services/CheckinServiceImpl.java
@@ -0,0 +1,75 @@
+package com.revature.services;
+
+import com.revature.entities.Associate;
+import com.revature.entities.Checkin;
+import com.revature.entities.Manager;
+import com.revature.exceptions.AlreadyCheckedInException;
+import com.revature.exceptions.AlreadyCheckedOutException;
+import com.revature.exceptions.NotCheckedInException;
+import com.revature.repositories.CheckinRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by mnikitin on 6/1/17.
+ */
+public class CheckinServiceImpl implements CheckinService {
+
+    @Autowired
+    private CheckinRepo checkinRepo;
+
+    @Override
+    public void approveCheckin(Manager approvingManager, Checkin checkin) {
+        checkin.setApprovedBy(approvingManager);
+        checkin.setApproveTime(LocalDateTime.now());
+        checkinRepo.save(checkin);
+    }
+
+    @Override
+    public boolean hasCheckedInToday(Associate associate) {
+        Set<Checkin> checkins = checkinRepo.getAllByCheckinTimeBetween(
+                LocalDateTime.of(
+                        LocalDate.now(), LocalTime.MIDNIGHT
+                ), LocalDateTime.now()
+        );
+        return (checkins != null && checkins.size() != 0);
+    }
+
+    @Override
+    public void checkIn(Associate associate) throws AlreadyCheckedInException {
+        // Have we already checked in today?
+        Set<Checkin> checkins = checkinRepo.getAllByCheckinTimeBetween(
+                LocalDateTime.of(
+                        LocalDate.now(), LocalTime.MIDNIGHT
+                ), LocalDateTime.now()
+        );
+        if(checkins != null && checkins.size() != 0)
+            throw new AlreadyCheckedInException();
+        Checkin checkin = new Checkin();
+        checkin.setAssociate(associate);
+        checkin.setCheckinTime(LocalDateTime.now());
+        checkinRepo.save(checkin);
+    }
+
+    @Override
+    public void checkOut(Associate associate) throws AlreadyCheckedOutException, NotCheckedInException {
+        Set<Checkin> checkins = checkinRepo.getAllByCheckinTimeBetween(
+                LocalDateTime.of(LocalDate.now(), LocalTime.MIDNIGHT),
+                LocalDateTime.now()
+        );
+        if(checkins == null || checkins.size() == 0)
+            throw new NotCheckedInException();
+        // We assume that there's exactly one, but don't complain if there's more than one for today.
+        for (Checkin checkin: checkins) {
+            if(checkin.getCheckoutTime() != null)
+                throw new AlreadyCheckedOutException();
+            checkin.setCheckoutTime(LocalDateTime.now());
+        }
+        checkinRepo.save(checkins);
+    }
+}
