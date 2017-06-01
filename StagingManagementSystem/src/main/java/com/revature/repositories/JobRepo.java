92f667a30140df7d621cd1e86e5838b769aa8121
Kota Delos Reyes <kdelosre>
2017-06-01 08:08:06 -0700

    added job service

---
+++ StagingManagementSystem/src/main/java/com/revature/repositories/JobRepo.java
@@ -0,0 +1,9 @@
+package com.revature.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.revature.entities.Job;
+
+public interface JobRepo extends JpaRepository<Job, Long>{
+	Job findById(long id);
+}
