977c43eb7b80085939e701f4988294bfa6fa7d43
Mykola Nikitin <mykola.nikitin.1995@gmail.com>
2017-06-01 12:57:47 -0400

    Cleaned up extra import

--- StagingManagementSystem/src/main/java/com/revature/services/CheckinServiceImpl.java
+++ StagingManagementSystem/src/main/java/com/revature/services/CheckinServiceImpl.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.List;
 import java.util.Set;
 
 /**
