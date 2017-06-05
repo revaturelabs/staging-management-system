4b7afdd1eaead557e378e387574177e4e4a8369e
Yushi Canastra <ycanastra@gmail.com>
2017-06-05 14:59:04 -0400

    add a way to edit portfolio url

--- StagingManagementSystem/src/main/resources/static/associate/profile/profile.js
+++ StagingManagementSystem/src/main/resources/static/associate/profile/profile.js
@@ -2,6 +2,7 @@ const profileCtrl = ($scope) => {
   $scope.name = 'Yushi Canastra';
   $scope.batchType = 'Java';
   $scope.portfolioUrl = 'http://www.example-portfolio.com/';
+  $scope.portfolioUrlInput = '';
   $scope.skills = ['Java', 'Spring', 'Hibernate', 'Servlets', 'JSP'];
   $scope.additionalSkills = ['hello', 'poop'];
   $scope.additionalSkillsInput = '';
@@ -10,6 +11,14 @@ const profileCtrl = ($scope) => {
   $scope.toggleSkillsModal = () => {
     $scope.additionalSkillsInput = $scope.additionalSkills.join(',');
     $('#additionalSkillsModal').modal('show');
+  };
+  $scope.openPortfolioUrlModal = () => {
+    $scope.portfolioUrlInput = $scope.portfolioUrl;
+    $('#portfolioUrlModal').modal('show');
+  };
+  $scope.submitPortfolioUrl = () => {
+    $scope.portfolioUrl = $scope.portfolioUrlInput;
+    $('#portfolioUrlModal').modal('hide');
   };
   $scope.submitSkills = () => {
     $scope.additionalSkills = $scope.additionalSkillsInput.split(',')
