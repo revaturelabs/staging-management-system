9bdb5ae1daae55273513ba151050a884798360a4
kshah007 <knshah045@gmail.com>
2017-06-05 08:59:34 -0400

    new changes to manager

---
+++ StagingManagementSystem/src/main/java/com/revature/controllers/rest/ManagerControllerImpl.java
@@ -0,0 +1,84 @@
+package com.revature.controllers.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.entities.Manager;
+import com.revature.services.ManagerService;
+
+@RestController
+@RequestMapping("manager")
+public class ManagerControllerImpl {
+	
+	@Autowired
+	private ManagerService managerService;
+	
+	public ManagerControllerImpl(ManagerService managerService){
+		super();
+		this.managerService = managerService;
+	}
+	
+	/**
+	 * When called this will always persist a unique manager in the database.
+	 * 
+	 * @param manager - manager to be persisted.
+	 */
+	@PostMapping
+	public void addManager(@RequestBody Manager manager) {
+		manager.setId((long) 0);
+		managerService.add(manager);
+	}
+	
+	/**
+	 * Deletes manager with location.id
+	 * 
+	 * @param manager - holds the id to be deleted
+	 */
+	@DeleteMapping
+	public void deleteManager(@RequestBody Manager manager) {
+		managerService.delete(manager);
+	}
+	
+	/**
+	 * If the id exists, updates information.
+	 * else creates a new row with genrated id.
+	 * 
+	 * @param manager - data to be persisted.
+	 */
+	@PutMapping
+	public void updateManager(@RequestBody Manager manager) {
+		managerService.update(manager);
+	}
+	
+	/**
+	 * Gets a manager with id.
+	 * 
+	 * @param id - id of manager to be retrieved.
+	 * @return manager object from dataBase.
+	 */
+	@GetMapping("/{id}")
+	public Manager findById(@PathVariable long id) {
+		return managerService.findById(id);
+	}
+	
+	/**
+	 * Gets all managers.
+	 * 
+	 * @param all
+	 * @return all manager objects from dataBase.
+	 */
+	@GetMapping("/all")
+	public List<Manager> findAll() {
+		return managerService.getAll();
+	}
+
+}
