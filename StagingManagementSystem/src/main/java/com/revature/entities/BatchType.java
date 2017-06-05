0df7be9b826459195487c625fc3f59132a7429de
Paul Maksimovich <paulmaksimovich@gmail.com>
2017-06-05 10:39:42 -0400

    Marked validation on all entities

--- StagingManagementSystem/src/main/java/com/revature/entities/BatchType.java
+++ StagingManagementSystem/src/main/java/com/revature/entities/BatchType.java
@@ -1,5 +1,6 @@
 package com.revature.entities;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.Column;
@@ -12,65 +13,80 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.revature.exceptions.SmsCustomException;
+import com.revature.markers.SmsValidatable;
 
 /**
  * Created by mnikitin on 5/31/17.
  */
 @Entity
 @Table(name = "BATCH_TYPES")
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-public class BatchType {
-    @Id
-    @Column(name="BATCH_TYPE_ID")
-    @SequenceGenerator(name="BATCH_TYPE_ID_SEQ", sequenceName="BATCH_TYPE_ID_SEQ")
-    @GeneratedValue(generator="BATCH_TYPE_ID_SEQ", strategy=GenerationType.SEQUENCE)
-    private long id;
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+public class BatchType implements SmsValidatable {
+	@Id
+	@Column(name = "BATCH_TYPE_ID")
+	@SequenceGenerator(name = "BATCH_TYPE_ID_SEQ", sequenceName = "BATCH_TYPE_ID_SEQ")
+	@GeneratedValue(generator = "BATCH_TYPE_ID_SEQ", strategy = GenerationType.SEQUENCE)
+	private long id;
 
-    @Column(name="BATCH_TYPE")
-    private String type;
+	@Column(name = "BATCH_TYPE")
+	private String type;
 
-    @OneToMany(mappedBy = "batchType")
-    private Set<InterviewQuestion> interviewQuestions;
+	@OneToMany(mappedBy = "batchType")
+	private Set<InterviewQuestion> interviewQuestions;
 
-    public BatchType(){
-        // Default constructor of uselessness.
-    }
-    public BatchType(long id, String type) {
-        this.id = id;
-        this.type = type;
-    }
+	public BatchType() {
+		super();
+		this.interviewQuestions = new HashSet<InterviewQuestion>();
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+	public BatchType(long id, String type, Set<InterviewQuestion> interviewQuestions) {
+		super();
+		this.id = id;
+		this.type = type;
+		this.interviewQuestions = interviewQuestions;
+	}
 
-        BatchType batchType = (BatchType) o;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
 
-        if (id != batchType.id) return false;
-        return type.equals(batchType.type);
-    }
+		BatchType batchType = (BatchType) o;
 
-    @Override
-    public int hashCode() {
-        int result = (int) (id ^ (id >>> 32));
-        result = 31 * result + type.hashCode();
-        return result;
-    }
+		if (id != batchType.id)
+			return false;
+		return type.equals(batchType.type);
+	}
 
-    public long getId() {
-        return id;
-    }
+	@Override
+	public int hashCode() {
+		int result = (int) (id ^ (id >>> 32));
+		result = 31 * result + type.hashCode();
+		return result;
+	}
 
-    public void setId(long id) {
-        this.id = id;
-    }
+	public long getId() {
+		return id;
+	}
 
-    public String getType() {
-        return type;
-    }
+	public void setId(long id) {
+		this.id = id;
+	}
 
-    public void setType(String type) {
-        this.type = type;
-    }
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Override
+	public void validate() throws SmsCustomException {
+		// TODO Validate your members.
+
+	}
 }
