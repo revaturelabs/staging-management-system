709c95c06ddf33efb73f3dba67a8f47bdc557a53
Paul Maksimovich <paulmaksimovich@gmail.com>
2017-06-09 16:17:01 -0400

    save

--- StagingManagementSystem/src/main/java/com/revature/test/TestClassOne.java
+++ StagingManagementSystem/src/main/java/com/revature/test/TestClassOne.java
@@ -1,10 +1,5 @@
 package com.revature.test;
 
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
-
-@Component
-@PropertySource("classpath:myproperties.properties")
 public class TestClassOne{
 
 }
