f6c33d08afe9a85670ba517e990bd08d6f4f7ee8
Mykola Nikitin <mykola.nikitin.1995@gmail.com>
2017-06-09 11:31:46 -0400

    Actually got logging working, good god.

--- StagingManagementSystem/src/main/java/com/revature/util/LoggingAspect.java
+++ StagingManagementSystem/src/main/java/com/revature/util/LoggingAspect.java
@@ -3,25 +3,21 @@ package com.revature.util;
 import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
-import org.springframework.stereotype.Component;
 
 @Aspect
-@Component
+//@Component
 public class LoggingAspect {;
     private Logger log = Logger.getRootLogger();
 
-    @NoLogging
-    @Pointcut("execution(* com.revature.controllers..*(..))" +
-            "&& !execution(* com.revature.util.*.*(..))")
+    @Pointcut("execution(* com.revature..*(..)) && !execution(* com.revature.config.AspectJConfiguration.*(..))")
     public void mostEverything(){/*Nope!~*/}
 
-    @NoLogging
     @Before("mostEverything()")
     public void logCall(JoinPoint joinPoint){
+        System.out.println("AAA");
         log.trace("Call made to: " + joinPoint.getSignature().getName());
     }
 
-    @NoLogging
     @AfterThrowing("mostEverything()")
     public void thrown(JoinPoint joinPoint){
         log.error("Exception thrown: " + joinPoint.getSignature().getName());
