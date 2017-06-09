c260158997fbec59c4d6db17ce355d76e926c27e
Kota Delos Reyes <kdelosre>
2017-06-09 13:25:07 -0700

    everything but datetime :')

--- StagingManagementSystem/src/main/resources/static/manager-pages/create/batch.js
+++ StagingManagementSystem/src/main/resources/static/manager-pages/create/batch.js
@@ -1,6 +1,11 @@
 const batchCtrl = ($scope, $http) => {
 	console.log('starting');
 
+            $(function () {
+                $('#datetimepicker1').datetimepicker();
+            });
+
+
   $http.get('batchtype/all.json').then( (response) => {
 		// console.log(response.data[0].id)
 		console.log(response)
@@ -15,17 +20,9 @@ const batchCtrl = ($scope, $http) => {
   		console.log("failure")
   	})
 
-$scope.open = function($event) {
-        $event.preventDefault();
-        $event.stopPropagation();
 
-        $scope.obj = {
-          opened : true
-          };
-      };
 
   $scope.submit = () => {
-    $scope.batch.startDate = $filter('date')($scope.batch.startDate, "dd/MM/yyyy", "UTC");
   	let item = (JSON.stringify($scope.batch));
   	console.log(item)
   	let str = item.replace(/\\/g, '')
