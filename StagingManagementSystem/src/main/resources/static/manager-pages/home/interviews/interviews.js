f74fe9cbdc43fb621c4800b7e2b99815d1240c34
Blake <btkruppa513@gmail.com>
2017-06-11 17:54:13 -0400

    work done on the interviews in the manager home page

--- StagingManagementSystem/src/main/resources/static/manager-pages/home/interviews/interviews.js
+++ StagingManagementSystem/src/main/resources/static/manager-pages/home/interviews/interviews.js
@@ -1,18 +1,29 @@
 const interviewsCtrl = ($scope, $http) => {
-    console.log("started")
-    $http({
-        method : "GET",
-        url : "interviews/all"
-    }).then(function mySuccess(response) {
-        window.interviews = response.data
-        $scope.interviews = response.data;
-    }, function myError(response) {
-        console.log("error!");
-    });
+  window.scope = $scope
+  $http({
+      method : "GET",
+      url : "interviews/next-five-days"
+  }).then(function mySuccess(response) {
+      $scope.interviews = response.data;
+  }, function myError(response) {
+      console.log("error!");
+  });
 
-    $scope.interviewSelect = (interview) => {
-      $scope.interviewSideTable = { "interview": interview };
-    }
+  $http.get('interviewStatus/all').then((successResponse) => {
+    $scope.interviewStatuses = successResponse.data;
+  }, (failResponse) => {
+    console.log('failed to retreive interview statuses')
+  })
+
+  $scope.interviewSelect = (interview) => {
+    //give the interview the status object from the list of statuses
+    interview.interviewStatus = $scope.interviewStatuses.filter((status) => status.value === interview.interviewStatus.value)[0];
+    $scope.interviewSideTable = { "interview": interview };
+    $scope.idSelectedInterview = interview.id;
+
+    window.scope = $scope
+  }
+
 };
 
 export { interviewsCtrl };
