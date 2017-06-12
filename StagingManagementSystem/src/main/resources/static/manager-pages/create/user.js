d21859894d2213f3fd94830fd51f0ef6c0c6c3a6
Blake <btkruppa513@gmail.com>
2017-06-12 13:58:16 -0400

    cleaned up code

--- StagingManagementSystem/src/main/resources/static/manager-pages/create/user.js
+++ StagingManagementSystem/src/main/resources/static/manager-pages/create/user.js
@@ -1,35 +1,31 @@
 const userCtrl = ($scope, $http) => {
 
-  $http.get('batchtype/all.json').then( (response) => {
-		// console.log(response.data[0].id)
-		console.log(response.data)
-		$scope.posts = response.data
-  	}, () => {
-  		console.log("failure")
-  	})
+  $scope.user.type = 'manager'
 
+  $http.get('batchtype/all.json')
+    .then((response) => {
+		  $scope.posts = response.data
+  	}, () => {
+  		console.log('failure')
+    });
 
   $scope.submit = () => {
   	let item = JSON.stringify($scope.user);
   	//need 2 different post requests for manager and associate
-  	console.log(item);
-  	console.log($scope.user.type == 'associate');
   	if ($scope.user.type == 'associate'){
-  		console.log('in associate');
-  	$http.post('/associate', item).then( (response) => {
-  		console.log("success")
-  	}, () => {
-  		console.log("failure")
-  	})
-};
-	if ($scope.user.type == 'manager'){
-		console.log('in manager');
-  	$http.post('/manager', item).then( (response) => {
-  		console.log("success")
-  	}, () => {
-  		console.log("failure")
-  	})
-};
+    	$http.post('/associate', item).then( (response) => {
+    		console.log('success')
+    	}, () => {
+    		console.log('failure')
+    	})
+    };
+  	if ($scope.user.type == 'manager'){
+    	$http.post('/manager', item).then( (response) => {
+    		console.log('success')
+    	}, () => {
+    		console.log('failure')
+    	})
+    };
   };
 };
 
