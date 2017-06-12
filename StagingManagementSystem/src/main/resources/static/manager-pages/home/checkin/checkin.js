23c69075b83224ef5adee95e4fbf6e559942c7ce
Cort <chighfi1@email.cpcc.edu>
2017-06-12 15:50:15 -0400

    adding changes

--- StagingManagementSystem/src/main/resources/static/manager-pages/home/checkin/checkin.js
+++ StagingManagementSystem/src/main/resources/static/manager-pages/home/checkin/checkin.js
@@ -7,7 +7,7 @@ const managerCheckinsCtrl = ($scope, $http) => {
 
     $http.get("checkin/allTodays").then(function(result) {
         $scope.checkins = result.data;
-        $scope.checkinDates = checkins.checkinTime;
+
 
     });
 
