e2048d6f616637972a26d2cb401b480e32658cb3
Cort <chighfi1@email.cpcc.edu>
2017-06-11 23:45:48 -0400

    commiting changes 11:45PM

--- StagingManagementSystem/src/main/resources/static/manager-pages/home/checkin/checkin.js
+++ StagingManagementSystem/src/main/resources/static/manager-pages/home/checkin/checkin.js
@@ -8,8 +8,10 @@ const managerCheckinsCtrl = ($scope, $http) => {
     $http.get("checkin/allTodays").then(function(result) {
         $scope.checkins = result.data;
         $scope.checkinDates = checkins.checkinTime;
-        $scope.isApproved = hasCheckedIn;
+
     });
+
+
 
 
 };
