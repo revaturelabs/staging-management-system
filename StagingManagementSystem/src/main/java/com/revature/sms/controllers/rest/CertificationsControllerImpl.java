c66342f091db4d1b75a9e5881e028857fe8d48d5
harjit <harjitsingh@harjitsinghs-MacBook-Air.local>
2017-08-18 11:49:43 -0400

    certifications controller

--- StagingManagementSystem/src/main/java/com/revature/sms/controllers/rest/CertificationsControllerImpl.java
+++ StagingManagementSystem/src/main/java/com/revature/sms/controllers/rest/CertificationsControllerImpl.java
@@ -1,13 +1,18 @@
 package com.revature.sms.controllers.rest;
 
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.sms.entities.Certifications;
+
 import com.revature.sms.services.CertificationsService;
 
 @RestController
@@ -23,4 +28,20 @@ public class CertificationsControllerImpl {
 		
 	}
 	
+	@GetMapping("/{id}")
+	public Certifications getCert(@PathVariable long cert_id){
+		return certService.getById(cert_id);
+	}
+	
+	
+@PostMapping
+public void add(@RequestBody Certifications certifications, @PathVariable String cert_type){
+	certifications.setCert_type(cert_type);
+	certService.add(certifications);
+}
+
+@GetMapping("/associate/{associateId}")
+public Set<Certifications> findByAssociate(@PathVariable long id) {
+	return certService.findByAssociate(id);
+}
 }
