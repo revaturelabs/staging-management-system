51cb45a6a9d8f70f79fe2b290df3ce6a6380ce94
Richard <richard.orr@revature.com>
2017-08-24 13:33:35 -0400

    hello

--- StagingManagementSystem/src/main/java/com/revature/sms/controllers/rest/TestImpl.java
+++ StagingManagementSystem/src/main/java/com/revature/sms/controllers/rest/TestImpl.java
@@ -1,15 +1,35 @@
 package com.revature.sms.controllers.rest;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.sms.entities.Batch;
+import com.revature.sms.repositories.BatchRepo;
+import com.revature.sms.repositories.SalesforceRepo;
 
 @RestController
 @RequestMapping("test")
 public class TestImpl {
 
+	@Autowired
+	private SalesforceRepo sd;
+	
+	@Autowired
+	private BatchRepo bd;
+	
     @GetMapping("/testone")
     public void permissionTest() {
-
+    }
+    
+    @GetMapping("/batches")
+    public void getBatches()
+    {
+    	List<Batch> batches = sd.getRelevantBatches();
+    	bd.save(batches);
+    	
     }
 }
