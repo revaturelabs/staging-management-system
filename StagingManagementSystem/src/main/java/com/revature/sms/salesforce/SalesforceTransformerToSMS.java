e657bb10c39d2acdebc59ad6d552d390392a635d
Richard <richard.orr@revature.com>
2017-09-01 17:01:03 -0400

    minor null pointer problem

--- StagingManagementSystem/src/main/java/com/revature/sms/salesforce/SalesforceTransformerToSMS.java
+++ StagingManagementSystem/src/main/java/com/revature/sms/salesforce/SalesforceTransformerToSMS.java
@@ -163,7 +163,7 @@ public class SalesforceTransformerToSMS implements SalesforceTransformer {
 
 		// Check if there was an old status
 		AssociatesStatus currentStatus = associate.getAssociateStatus();
-		if (currentStatus != null) {
+		if (currentStatus != null && currentStatus.getStatus() !=null) {
 			// We have to update a few things
 			if (status.equals("STAGING") && currentStatus.getStatus().equals("PROJECT")) {
 				// status should actually be BENCH
